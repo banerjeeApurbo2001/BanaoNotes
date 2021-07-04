57eb72532dd4623fb3f1b6ec968f6acb798dcdd4
Apurba Banerjee <apurbosonu18@gmail.com>
2021-07-04 13:21:29 +0530

    Added a splash screen for the app and routed the home screen via splash in main.dart

--- lib/ui/pages/splash.dart
+++ lib/ui/pages/splash.dart
@@ -1,17 +1,45 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text("Banao Notes"),
+import 'notes_home.dart';
 
-      ),
-    );
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 4),
+            () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => NotesHomPage())));
   }
 
-  delay() async{
-    Duration( seconds: 3);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+              child: Image.asset('assets/splash.png',
+              height: 200,
+              width: 200,),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+              child: Text("Banao Notes",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30,color: Colors.blue)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
